ae4c7809ab9051fac31933852a098531372772ce
Marcos Volpi <marcos@marcosvolpi.com>
2022-12-09 20:08:04 -0300

    Add mongodb configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/challenge_kenility', {
+      useNewUrlParser: true
+    }),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
