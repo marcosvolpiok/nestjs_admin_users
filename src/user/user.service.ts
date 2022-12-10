006dbbcf870bc87fd7b954e56a120ea22cb12cb9
Marcos Volpi <marcos@marcosvolpi.com>
2022-12-10 07:28:33 -0300

    Remove unnecesaries methods

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -16,30 +16,10 @@ export class UserService {
         const user = await this.userModel.find();
         return user;
     }
-    /*
-    // Get a single Product
-    async getProduct(productID: string): Promise<Product> {
-        const product = await this.productModel.findById(productID); 
-        return product;
-    }
-*/
+    
     // Post a single product
     async createUser(createUserDTO: CreateUserDTO): Promise<User> {
         const newUser = new this.userModel(createUserDTO);
         return newUser.save();
     }
-/*
-    // Delete Product
-    async deleteProduct(productID: string): Promise<any> {
-        const deletedProduct = await this.productModel.findOneAndDelete(productID);
-        return deletedProduct;
-    }
-
-    // Put a single product
-    async updateProduct(productID: string, createProductDTO: CreateProductDTO): Promise<Product> {
-        const updatedProduct = await this.productModel
-                            .findByIdAndUpdate(productID, createProductDTO, {new: true});
-        return updatedProduct;
-    }
-*/
 }
