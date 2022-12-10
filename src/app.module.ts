a65a5a2557a8ddbcd1a470d6c465801629fc0ef0
Marcos Volpi <marcos@marcosvolpi.com>
2022-12-10 07:33:59 -0300

    Add tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from "@nestjs/mongoose";
 
@@ -11,7 +9,7 @@ import { MongooseModule } from "@nestjs/mongoose";
     }),
     UserModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
