4582e85b4cf25f0af17d3ef091582fe025b38ae7
Marcos Volpi <marcos@marcosvolpi.com>
2022-12-15 09:55:58 -0300

    Fix

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,9 @@
 
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Res, HttpStatus } from '@nestjs/common';
 import { LocalAuthGuard } from '../auth/local-auth.guard';
 import { AuthService } from '../auth/auth.service';
 import { ResponseService } from '../response/response.service';
+import { ResponseInterface } from '../response/response.interface';
 
 @Controller()
 export class AuthController {
@@ -13,12 +14,14 @@ export class AuthController {
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  async login(@Request() req) {
+  async login(@Res() res, @Request() req) {
     try {
       const result = await this.authService.login(req.user);
       return this.responseService.getResponse(result, 'OK');
     } catch (error) {
-      return this.responseService.getResponse({}, 'ERROR', error.message);
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(
+        this.responseService.getResponse(error.message, 'ERROR')
+      );
     }
   } 
 }
