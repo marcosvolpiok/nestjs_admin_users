756e3d1c871be23f0193da43c8b5bf953d3ae6bc
Marcos Volpi <marcos@marcosvolpi.com>
2022-12-15 10:50:04 -0300

    Refactor

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -19,46 +19,69 @@ export class UserController {
 
     @Post('/')
     async createUser(@Res() res, @Body() createUserDTO: CreateUserDTO) {
+        let response;
+
         if (await this.userService.getUserByUsername(createUserDTO.user)) {
-            return res.status(HttpStatus.BAD_REQUEST).json({
+            response = res.status(HttpStatus.BAD_REQUEST).json({
                 message: 'User Already Exists',
             });
         } else {
             const user = await this.userService.createUser(createUserDTO);
             
-            return this.responseService.getResponse(
-                {user, message: 'User Successfully Created'}
-                , 'OK');
+            response = res.status(HttpStatus.CREATED).json(
+                this.responseService.getResponse(
+                    {user, message: 'User Successfully Created'}
+                    , 'OK'
+                )
+            )
         }
+
+        return response;
     }
 
     @UseGuards(JwtAuthGuard)
     @Put('/:id')
     async updateUser(@Res() res, @Param('id') id, @Body() createUserDTO: CreateUserDTO) {
+        let response;
+
         if (await this.userService.getUserById(id)) {
             const updatedUser = await this.userService.updateUser(id, createUserDTO);
 
-            return res.status(HttpStatus.OK).json(updatedUser);
+            response = res.status(HttpStatus.OK).json(
+                this.responseService.getResponse(
+                    updatedUser,
+                    'OK')
+            )
         } else {
-            return res.status(HttpStatus.NOT_FOUND).json({
+            response = res.status(HttpStatus.NOT_FOUND).json({
                 message: 'User Not Found',
             });
         }
+
+        return response;
     }
 
     @UseGuards(JwtAuthGuard)
     @Get('/')
     async getUsers(@Res() res) {
+        let response;
         const users = await this.userService.getUser();
 
-        return res.status(HttpStatus.OK).json(users);
+        response = res.status(HttpStatus.OK).json(
+            this.responseService.getResponse(
+                users,
+                'OK')
+        )
+        return response;
     }
 
     @UseGuards(JwtAuthGuard)
     @Put('avatar/:id')
     @UseInterceptors(FileInterceptor('file'))
     async uploadFile(@Res() res, @UploadedFile() file: Express.Multer.File, @Param('id') id) {
+        let response;
         const user = await this.userService.getUserById(id);
+
         if (user) {
             const result = await this.s3Service.uploadFile(file);
             if(result.Location) {
@@ -67,12 +90,18 @@ export class UserController {
                 }
                 const userUpdated = await this.userService.updateImage(id, updateImageDTO);
 
-                return res.status(HttpStatus.OK).json(userUpdated);
+                response = res.status(HttpStatus.OK).json(
+                    this.responseService.getResponse(
+                        userUpdated,
+                        'OK')
+                )
             }
         } else {
-            return res.status(HttpStatus.NOT_FOUND).json({
+            response = res.status(HttpStatus.NOT_FOUND).json({
                 message: 'User Not Found',
             });
         }
+
+        return response;
     }     
 }
